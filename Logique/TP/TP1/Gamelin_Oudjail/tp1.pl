08125e6927f9e812e9e181b22caa80ef4957bcf4
kevin gamelin <kevin-gamelin@hotmail.fr>
2016-01-14 10:05:00 +0100

    tp1 logique

--- Logique/TP/TP1/Gamelin_Oudjail/tp1.pl
+++ Logique/TP/TP1/Gamelin_Oudjail/tp1.pl
@@ -21,18 +21,67 @@ salle1(X, Y) :- contenu(X), contenu(Y), not(pancarte1(X, Y)), not(pancarte2(X, Y
 
 % Question 5
 /*
-  Trace salle1 (pancarte1, pancarte2) :
-   [debug] 5 ?- salle1(X, Y).
-   T Call: (7) salle1(_G2513, _G2514)
-   T Call: (8) pancarte1(_G2513, _G2514)
-   T Exit: (8) pancarte1(tigre, _G2514)
-   T Call: (8) pancarte2(tigre, _G2514)
-   T Fail: (8) pancarte2(tigre, _G2514)
-   T Redo: (8) pancarte1(_G2513, _G2514)
-   T Exit: (8) pancarte1(_G2513, princesse)
-   T Call: (8) pancarte2(_G2513, princesse)
-   T Exit: (8) pancarte2(princesse, princesse)
-   T Exit: (7) salle1(princesse, princesse)
+  [debug]  ?- salle1(X,Y).
+  T Call: (6) salle1(_G3803, _G3804)
+  T Call: (7) contenu(_G3803)
+  T Exit: (7) contenu(tigre)
+  T Call: (7) contenu(_G3804)
+  T Exit: (7) contenu(tigre)
+  T Call: (7) pancarte1(tigre, tigre)
+  T Exit: (7) pancarte1(tigre, tigre)
+  T Call: (7) pancarte2(tigre, tigre)
+  T Fail: (7) pancarte2(tigre, tigre)
+  T Redo: (7) pancarte1(tigre, tigre)
+  T Fail: (7) pancarte1(tigre, tigre)
+  T Redo: (7) contenu(_G3804)
+  T Exit: (7) contenu(princesse)
+  T Call: (7) pancarte1(tigre, princesse)
+  T Exit: (7) pancarte1(tigre, princesse)
+  T Call: (7) pancarte2(tigre, princesse)
+  T Fail: (7) pancarte2(tigre, princesse)
+  T Redo: (7) pancarte1(tigre, princesse)
+  T Exit: (7) pancarte1(tigre, princesse)
+  T Call: (7) pancarte2(tigre, princesse)
+  T Fail: (7) pancarte2(tigre, princesse)
+  T Redo: (7) contenu(_G3803)
+  T Exit: (7) contenu(princesse)
+  T Call: (7) contenu(_G3804)
+  T Exit: (7) contenu(tigre)
+  T Call: (7) pancarte1(princesse, tigre)
+  T Fail: (7) pancarte1(princesse, tigre)
+  T Redo: (7) contenu(_G3804)
+  T Exit: (7) contenu(princesse)
+  T Call: (7) pancarte1(princesse, princesse)
+  T Exit: (7) pancarte1(princesse, princesse)
+  T Call: (7) pancarte2(princesse, princesse)
+  T Exit: (7) pancarte2(princesse, princesse)
+  T Exit: (6) salle1(princesse, princesse)
+  X = Y, Y = princesse ;
+  T Redo: (6) salle1(_G3803, _G3804)
+  T Call: (7) contenu(_G3803)
+  T Exit: (7) contenu(tigre)
+  T Call: (7) contenu(_G3804)
+  T Exit: (7) contenu(tigre)
+  T Call: (8) pancarte1(tigre, tigre)
+  T Exit: (8) pancarte1(tigre, tigre)
+  T Redo: (7) contenu(_G3804)
+  T Exit: (7) contenu(princesse)
+  T Call: (8) pancarte1(tigre, princesse)
+  T Exit: (8) pancarte1(tigre, princesse)
+  T Redo: (7) contenu(_G3803)
+  T Exit: (7) contenu(princesse)
+  T Call: (7) contenu(_G3804)
+  T Exit: (7) contenu(tigre)
+  T Call: (8) pancarte1(princesse, tigre)
+  T Fail: (8) pancarte1(princesse, tigre)
+  T Call: (8) pancarte2(princesse, tigre)
+  T Exit: (8) pancarte2(princesse, tigre)
+  T Redo: (7) contenu(_G3804)
+  T Exit: (7) contenu(princesse)
+  T Call: (8) pancarte1(princesse, princesse)
+  T Exit: (8) pancarte1(princesse, princesse)
+  T Fail: (6) salle1(_G3803, _G3804)
+  false.
 */
 
 % Deuxième  salle
@@ -54,21 +103,60 @@ salle2(X, Y) :- contenu(X), contenu(Y), pancarte22(X, Y), not(pancarte21(X, Y)).
 
 % Question 8
 /*
-  T Call: (7) salle2(_G2513, _G2514)
-  T Call: (8) pancarte21(_G2513, _G2514)
-  T Exit: (8) pancarte21(princesse, tigre)
-  T Call: (9) pancarte22(princesse, tigre)
-  T Exit: (9) pancarte22(princesse, tigre)
-  T Redo: (7) salle2(_G2513, _G2514)
-  T Call: (8) pancarte22(_G2513, _G2514)
-  T Exit: (8) pancarte22(princesse, tigre)
-  T Call: (9) pancarte21(princesse, tigre)
-  T Exit: (9) pancarte21(princesse, tigre)
-  T Redo: (8) pancarte22(_G2513, _G2514)
-  T Exit: (8) pancarte22(tigre, princesse)
-  T Call: (9) pancarte21(tigre, princesse)
-  T Fail: (9) pancarte21(tigre, princesse)
-  T Exit: (7) salle2(tigre, princesse)
+  [debug]  ?- salle2(X,Y).
+   T Call: (6) salle2(_G3803, _G3804)
+   T Call: (7) contenu(_G3803)
+   T Exit: (7) contenu(tigre)
+   T Call: (7) contenu(_G3804)
+   T Exit: (7) contenu(tigre)
+   T Call: (7) pancarte21(tigre, tigre)
+   T Fail: (7) pancarte21(tigre, tigre)
+   T Redo: (7) contenu(_G3804)
+   T Exit: (7) contenu(princesse)
+   T Call: (7) pancarte21(tigre, princesse)
+   T Fail: (7) pancarte21(tigre, princesse)
+   T Redo: (7) contenu(_G3803)
+   T Exit: (7) contenu(princesse)
+   T Call: (7) contenu(_G3804)
+   T Exit: (7) contenu(tigre)
+   T Call: (7) pancarte21(princesse, tigre)
+   T Exit: (7) pancarte21(princesse, tigre)
+   T Call: (8) pancarte22(princesse, tigre)
+   T Exit: (8) pancarte22(princesse, tigre)
+   T Redo: (7) contenu(_G3804)
+   T Exit: (7) contenu(princesse)
+   T Call: (7) pancarte21(princesse, princesse)
+   T Fail: (7) pancarte21(princesse, princesse)
+   T Redo: (6) salle2(_G3803, _G3804)
+   T Call: (7) contenu(_G3803)
+   T Exit: (7) contenu(tigre)
+   T Call: (7) contenu(_G3804)
+   T Exit: (7) contenu(tigre)
+   T Call: (7) pancarte22(tigre, tigre)
+   T Fail: (7) pancarte22(tigre, tigre)
+   T Redo: (7) contenu(_G3804)
+   T Exit: (7) contenu(princesse)
+   T Call: (7) pancarte22(tigre, princesse)
+   T Exit: (7) pancarte22(tigre, princesse)
+   T Call: (8) pancarte21(tigre, princesse)
+   T Fail: (8) pancarte21(tigre, princesse)
+   T Exit: (6) salle2(tigre, princesse)
+  X = tigre,
+  Y = princesse ;
+   T Redo: (7) contenu(_G3803)
+   T Exit: (7) contenu(princesse)
+   T Call: (7) contenu(_G3804)
+   T Exit: (7) contenu(tigre)
+   T Call: (7) pancarte22(princesse, tigre)
+   T Exit: (7) pancarte22(princesse, tigre)
+   T Call: (8) pancarte21(princesse, tigre)
+   T Exit: (8) pancarte21(princesse, tigre)
+   T Redo: (7) contenu(_G3804)
+   T Exit: (7) contenu(princesse)
+   T Call: (7) pancarte22(princesse, princesse)
+   T Fail: (7) pancarte22(princesse, princesse)
+   T Fail: (6) salle2(_G3803, _G3804)
+  false.
 */
 
 % Question 9
@@ -97,8 +185,6 @@ contenu2(vide).
 
 pancarte31(princesse, tigre, vide).
 pancarte31(tigre, princesse, vide).
-pancarte31(_, _, vide).
-
 
 pancarte32(tigre, princesse, vide).
 pancarte32(tigre, vide, princesse).
@@ -109,17 +195,14 @@ pancarte33(princesse, tigre, vide).
 
 
 porte1(P1, P2, P3) :- pancarte31(P1, P2, P3), pancarte33(P1, P2, P3), not(pancarte32(P1, P2, P3)).
-% porte1(P1, P2, P3) :- not(pancarte31(P1, P2, P3)), not(pancarte33(P1, P2, P3)), pancarte32(P1, P2, P3).
 
 porte2(P1, P2, P3) :- pancarte32(P1, P2, P3), not(pancarte31(P1, P2, P3)), not(pancarte33(P1, P2, P3)).
-% porte2(P1, P2, P3) :- not(pancarte32(P1, P2, P3)), pancarte31(P1, P2, P3), pancarte33(P1, P2, P3).
 
 porte3(P1, P2, P3) :- pancarte33(P1, P2, P3), pancarte31(P1, P2, P3), not(pancarte32(P1, P2, P3)).
-%porte3(P1, P2, P3) :- not(pancarte33(P1, P2, P3)), not(pancarte31(P1, P2, P3)), pancarte32(P1, P2, P3).
 
 % Question 12
-salle3(P1, P2, P3) :- contenu2(P1), contenu2(P2), contenu2(P3), not(porte1(P1, P2, P3)), porte2(P1, P2, P3), not(porte3(P1, P2, P3)).
-salle3(P1, P2, P3) :- contenu2(P1), contenu2(P2), contenu2(P3), porte1(P1, P2, P3), not(porte2(P1, P2, P3)), porte3(P1, P2, P3).
+salle3(P1, P2, P3) :- not(porte1(P1, P2, P3)), porte2(P1, P2, P3), not(porte3(P1, P2, P3)).
+salle3(P1, P2, P3) :- porte1(P1, P2, P3), not(porte2(P1, P2, P3)), porte3(P1, P2, P3).
 
 % Question 13
 /*
@@ -131,35 +214,49 @@ salle3(P1, P2, P3) :- contenu2(P1), contenu2(P2), contenu2(P3), porte1(P1, P2, P
 
 % Question 14
 /*
- T Call: (7) salle3(_G3190, _G3191, _G3192)
- T Call: (9) porte1(_G3190, _G3191, _G3192)
- T Call: (10) pancarte31(_G3190, _G3191, _G3192)
- T Exit: (10) pancarte31(princesse, tigre, vide)
- T Call: (10) pancarte33(princesse, tigre, vide)
- T Exit: (10) pancarte33(princesse, tigre, vide)
- T Call: (11) pancarte32(princesse, tigre, vide)
- T Fail: (11) pancarte32(princesse, tigre, vide)
- T Exit: (9) porte1(princesse, tigre, vide)
- T Redo: (7) salle3(_G3190, _G3191, _G3192)
- T Call: (8) porte1(_G3190, _G3191, _G3192)
- T Call: (9) pancarte31(_G3190, _G3191, _G3192)
- T Exit: (9) pancarte31(princesse, tigre, vide)
- T Call: (9) pancarte33(princesse, tigre, vide)
- T Exit: (9) pancarte33(princesse, tigre, vide)
- T Call: (10) pancarte32(princesse, tigre, vide)
- T Fail: (10) pancarte32(princesse, tigre, vide)
- T Exit: (8) porte1(princesse, tigre, vide)
- T Call: (9) porte2(princesse, tigre, vide)
- T Call: (10) pancarte32(princesse, tigre, vide)
- T Fail: (10) pancarte32(princesse, tigre, vide)
- T Fail: (9) porte2(princesse, tigre, vide)
- T Call: (8) porte3(princesse, tigre, vide)
- T Call: (9) pancarte33(princesse, tigre, vide)
- T Exit: (9) pancarte33(princesse, tigre, vide)
- T Call: (9) pancarte31(princesse, tigre, vide)
- T Exit: (9) pancarte31(princesse, tigre, vide)
- T Call: (10) pancarte32(princesse, tigre, vide)
- T Fail: (10) pancarte32(princesse, tigre, vide)
- T Exit: (8) porte3(princesse, tigre, vide)
- T Exit: (7) salle3(princesse, tigre, vide)
+  [debug]  ?- salle3(X,Y,Z).
+   T Call: (6) salle3(_G3803, _G3804, _G3805)
+   T Call: (8) porte1(_G3803, _G3804, _G3805)
+   T Call: (9) pancarte31(_G3803, _G3804, _G3805)
+   T Exit: (9) pancarte31(princesse, tigre, vide)
+   T Call: (9) pancarte33(princesse, tigre, vide)
+   T Exit: (9) pancarte33(princesse, tigre, vide)
+   T Call: (10) pancarte32(princesse, tigre, vide)
+   T Fail: (10) pancarte32(princesse, tigre, vide)
+   T Exit: (8) porte1(princesse, tigre, vide)
+   T Redo: (6) salle3(_G3803, _G3804, _G3805)
+   T Call: (7) porte1(_G3803, _G3804, _G3805)
+   T Call: (8) pancarte31(_G3803, _G3804, _G3805)
+   T Exit: (8) pancarte31(princesse, tigre, vide)
+   T Call: (8) pancarte33(princesse, tigre, vide)
+   T Exit: (8) pancarte33(princesse, tigre, vide)
+   T Call: (9) pancarte32(princesse, tigre, vide)
+   T Fail: (9) pancarte32(princesse, tigre, vide)
+   T Exit: (7) porte1(princesse, tigre, vide)
+   T Call: (8) porte2(princesse, tigre, vide)
+   T Call: (9) pancarte32(princesse, tigre, vide)
+   T Fail: (9) pancarte32(princesse, tigre, vide)
+   T Fail: (8) porte2(princesse, tigre, vide)
+   T Call: (7) porte3(princesse, tigre, vide)
+   T Call: (8) pancarte33(princesse, tigre, vide)
+   T Exit: (8) pancarte33(princesse, tigre, vide)
+   T Call: (8) pancarte31(princesse, tigre, vide)
+   T Exit: (8) pancarte31(princesse, tigre, vide)
+   T Call: (9) pancarte32(princesse, tigre, vide)
+   T Fail: (9) pancarte32(princesse, tigre, vide)
+   T Exit: (7) porte3(princesse, tigre, vide)
+   T Exit: (6) salle3(princesse, tigre, vide)
+  X = princesse,
+  Y = tigre,
+  Z = vide ;
+   T Redo: (8) pancarte31(_G3803, _G3804, _G3805)
+   T Exit: (8) pancarte31(tigre, princesse, vide)
+   T Call: (8) pancarte33(tigre, princesse, vide)
+   T Exit: (8) pancarte33(tigre, princesse, vide)
+   T Call: (9) pancarte32(tigre, princesse, vide)
+   T Exit: (9) pancarte32(tigre, princesse, vide)
+   T Fail: (7) porte1(_G3803, _G3804, _G3805)
+   T Fail: (6) salle3(_G3803, _G3804, _G3805)
+  false.
+
 */
