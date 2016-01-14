dca9b2a866fe0daf4a4111f6a264f8715041a3f4
zeroCrowsky <veis.oudjail@gmail.com>
2016-01-14 22:13:19 +0100

    PF alors qu'il est 22 h 12 et que Maxime c'est venger

--- PF/TP/TP1/oudjail/test.hs
+++ PF/TP/TP1/oudjail/test.hs
@@ -1,11 +1,11 @@
 module Main where
 
 -- Fonction sommeQ1, fait une somme des nombres de 1 à n
-sommeQ1:: (Num a, Enum a) => a -> a
+sommeQ1 :: (Num a, Enum a) => a -> a
 sommeQ1 n = sum [1..n]
 
 -- Fonction sommeDeXaY, fait une somme des entier ce trouvant entre x et y inclus
-sommeDeXaY::  Int -> Int -> Int -- Integer regroupe les deux contrainte (Num a, Ord a)
+sommeDeXaY ::  Int -> Int -> Int -- Integer regroupe les deux contrainte (Num a, Ord a)
 sommeDeXaY x y =
   if x >= y then
     x
@@ -13,25 +13,25 @@ sommeDeXaY x y =
     x + sommeDeXaY (x+1) y
 
 -- Fonction somme, fait une somme des entier de la liste passer en param
-somme:: Num a => [a] -> a
+somme :: Num a => [a] -> a
 somme [] = 0
 somme (x:xs) = x + somme xs
 
 -- Fonction mlast, donne le derniere element de la liste
-mlast:: [a] -> a
+mlast :: [a] -> a
 mlast [a] = a
 mlast xs = mlast (tail xs)
 
 -- Fonction minit, donne la liste passer en param sans le dernier element
-minit:: [a] -> [a]
+minit :: [a] -> [a]
 minit xs = reverse (tail (reverse xs))
 
 -- Fonction getElmSimple, donne le i-eme element de la liste passer en param
-getElmSimple:: [a] -> Int ->  a
+getElmSimple :: [a] -> Int ->  a
 getElmSimple l i = last (take (i+1) l)
 
 -- Fonction getElmRecA, donne le i-eme element de la liste passer en param (V2)
-getElmRecA:: [a] -> Int -> Int->  a
+getElmRecA :: [a] -> Int -> Int->  a
 getElmRecA l cpt i =
   if cpt == i then
     head l
@@ -39,45 +39,45 @@ getElmRecA l cpt i =
     getElmRecA (tail l) (cpt+1) i
 
 -- Fonction getElmRec, encapsule l'initialisation de la fonction getElmRecA
-getElmRec:: [a] -> Int ->  a
+getElmRec :: [a] -> Int ->  a
 getElmRec l i = getElmRecA l 0 i
 
 -- Fonction mplus, permet de concatener 2 liste
-mplus:: [a] -> [a] -> [a]
+mplus :: [a] -> [a] -> [a]
 mplus [] l = l
 mplus l1 l2 = (head l1):(mplus (tail l1) l2) -- Motif non utilise, pb Indent
 
-mmconcat:: [[a]] -> [a]
+mmconcat :: [[a]] -> [a]
 mmconcat [] = []
 mmconcat (xs : xss) = xs ++ (mmconcat xss) -- Probleme de synthaxe avec mplus
 
-mmap:: (a -> b) -> [a] -> [b]
+mmap :: (a -> b) -> [a] -> [b]
 mmap _ [] = []
 mmap fct (x : xs) = (fct x) : (mmap fct xs)
 
 -- Fonction numberOne, permet de transfomer tout objet en le chiffre un
-numberOne:: a -> Int
+numberOne :: a -> Int
 numberOne _ = 1
 
 -- Fonction mlenght, donne la taille d'une liste (containte etant l'utilisation des fct map, sum)
-mlenght:: [a] -> Int
+mlenght :: [a] -> Int
 mlenght l = sum (map numberOne l)
 
-applyFctN:: (a -> a) -> a -> Int -> a
+applyFctN :: (a -> a) -> a -> Int -> a
 applyFctN _ x 0 = x
 applyFctN fct x i = applyFctN fct (fct x) (i-1)
 
-createListeFctN:: (a -> a) -> a -> Int -> [a]
+createListeFctN :: (a -> a) -> a -> Int -> [a]
 createListeFctN _ _ 0 = []
 createListeFctN fct x i = (createListeFctN fct x (i-1)) ++ [(applyFctN fct x (i-1))]
 
-createListeFctNWithIterate:: (a -> a) -> a -> Int -> [a]
+createListeFctNWithIterate :: (a -> a) -> a -> Int -> [a]
 createListeFctNWithIterate fct x i = take i (iterate fct x)
 
-increment:: Int -> Int
+increment :: Int -> Int
 increment x = x + 1
 
-createListeN:: Int -> [Int]
+createListeN :: Int -> [Int]
 createListeN n = createListeFctN increment 0 (n+1)
 
 -- Foncion main, fonction executer au chargement
--- PF/TP/TP2/oudjail/tp2.hs
+++ PF/TP/TP2/oudjail/tp2.hs
@@ -1,22 +1,28 @@
 module Main where
+import Graphics.Gloss
 
-alterne:: [a]  -> [a]
+
+alterne :: [a]  -> [a]
 alterne [] = []
 alterne [e] = [e]
 alterne (x1 : _ : xs) = x1 : (alterne xs)
 
-combine:: (a -> b -> c) -> [a] -> [b] -> [c]
+combine :: (a -> b -> c) -> [a] -> [b] -> [c]
 combine _ [] [] = []
 combine _ [] (_ : _) = []
 combine _ (_ : _) [] = []
 combine fct (x1 : xs1) (x2 : xs2) = (fct x1 x2) : (combine fct xs1 xs2)
 
-pasPascal:: [Int] -> [Int]
+pasPascal :: [Int] -> [Int]
 pasPascal [] = [1]
 pasPascal l = [1] ++ (zipWith (+) (tail l) (init l)) ++ [1]
 
-pascal:: [[Int]]
+pascal :: [[Int]]
 pascal = (iterate pasPascal [])
+
+pointAintercaler :: Point -> Point -> Point
+pointAintercaler (xA, xB) (yA, yB) = ((xA + xB)/2 + (yB - yA)/2, (yA + yB)/2 + (xA - xB)/2)
+
 
 
 main :: IO ()
