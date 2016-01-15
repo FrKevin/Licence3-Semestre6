aa377f1bbdd3e3d62412276bbec5b7bc1e709773
Kevin <kevin-gamelin@hotmail.fr>
2016-01-15 15:03:03 +0100

    projet OpenDev Breakout

--- PF/TP/TP2/gamelin/tp2.hs
+++ PF/TP/TP2/gamelin/tp2.hs
@@ -7,9 +7,9 @@ pointAintercaler (xA, xB) (yA, yB) = ((xA + xB)/2 + (yB - yA)/2, (
 
 pasDragon :: Path -> Path
 pasDragon [] = []
-pasDragon [xa] = [xa] -- Ou mettre un fail
+pasDragon [xa] = [xa]
 pasDragon [xa, xb] = xa : [pointAintercaler xa xb] ++ [xb]
-pasDragon (xa : xb : xc : xs) = xa : (pointAintercaler xa xb) : xb : (pointAintercaler xc xb) : xc : (pasDragon xs)
+pasDragon (xa : xb : xc : xs) = xa : (pointAintercaler xa xb) : xb : (pointAintercaler xc xb) : (pasDragon xc:xs)
 
 dragon :: Point -> Point -> [Path]
 dragon (xA, xB) (yA, yB) = (iterate pasDragon [(xA, xB),(yA, yB)])
