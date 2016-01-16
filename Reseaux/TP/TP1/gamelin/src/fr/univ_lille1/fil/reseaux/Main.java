aea27362ba22df4da4dc06914de9ccd7168b5df3
Kevin <kevin-gamelin@hotmail.fr>
2016-01-16 15:41:35 +0100

    update tp1 réseaux

--- Reseaux/TP/TP1/gamelin/src/fr/univ_lille1/fil/reseaux/Main.java
+++ Reseaux/TP/TP1/gamelin/src/fr/univ_lille1/fil/reseaux/Main.java
@@ -1,8 +1,18 @@
 package fr.univ_lille1.fil.reseaux;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
 
 	public static void main(String[] args) {
+		   try {
+               UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+           } catch (ClassNotFoundException ex) {
+           } catch (InstantiationException ex) {
+           } catch (IllegalAccessException ex) {
+           } catch (UnsupportedLookAndFeelException ex) {
+           }
 		Application app = new Application("224.0.0.1", 7654);
 	}
 
