d9fc0d23d9158ec4623d59395bb9a246d807b7a9
zeroCrowsky <veis.oudjail@gmail.com>
2016-02-05 13:47:46 +0100

    Sync OpenDev

--- PF/TP/TP4/oudjail/ArbreBinaire.hs
+++ PF/TP/TP4/oudjail/ArbreBinaire.hs
@@ -16,6 +16,7 @@ foldArbreBinaire :: (a -> b -> b -> b) -> b -> ArbreBinaire c a -> b
 foldArbreBinaire _ n Feuille = n
 foldArbreBinaire f n (Noeud _ v g d) = f v (foldArbreBinaire f n g) (foldArbreBinaire f n d)
 
+
 hauteur :: ArbreBinaire c a -> Int
 hauteur Feuille = 0
 hauteur (Noeud _ _ g d) = 1 + max (hauteur g) (hauteur d)
@@ -25,6 +26,14 @@ taille Feuille = 0
 taille (Noeud _ _ g d) = 1 + taille g + taille d
 
 hauteur' :: ArbreBinaire c a -> Int
+hauteur' = foldArbreBinaire (\_ y z -> 1 + max y z) 0
+
+taille' :: ArbreBinaire c a -> Integer
+taille' = foldArbreBinaire (\_ y z -> 1 + y + z) 0
+
+peigneGauche :: [(c, a)] -> ArbreBinaire c a
+peigneGauche = foldl (\ x (c, v) -> Noeud c v x Feuille) Feuille
+
 
 
 main :: IO ()
