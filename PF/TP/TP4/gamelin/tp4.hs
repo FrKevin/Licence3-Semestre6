4850583a476a131c886fc823a82c394d29b072ab
Kevin <kevin-gamelin@hotmail.fr>
2016-02-23 11:45:15 +0100

    PF TP4

--- PF/TP/TP4/gamelin/tp4.hs
+++ PF/TP/TP4/gamelin/tp4.hs
@@ -16,6 +16,10 @@ foldrArbre:: (v -> a -> a -> a) -> a -> Arbre c v -> a
 foldrArbre _  acc Feuille = acc
 foldrArbre op acc (Noeud v c g d) = op v (foldrArbre op acc g) (foldrArbre op acc d)
 
+foldArbre' :: ((c,a) -> b -> b -> b) -> b -> Arbre c a -> b
+foldArbre' _ n Feuille         = n
+foldArbre' f n (Noeud c v g d) = f (v,c) (foldArbre' f n g) (foldArbre' f n d)
+
 hauteurRec:: Arbre c v -> Int
 hauteurRec Feuille = 0
 hauteurRec (Noeud _ _ g d) = 1 + max (hauteurRec g) (hauteurRec d)
@@ -47,6 +51,40 @@ repeat'= iterate id
 
 --Q11
 listeTuple =foldr (\ x y -> ( (), x) : y) [] ['a'..]
+
+--Q12
+aplatit :: Arbre c a -> [(c, a)]
+aplatit = foldArbre' (\x y z -> y ++ [x] ++ z) []
+
+--Q13
+element :: Eq a => a -> Arbre c a -> Bool
+element _ Feuille = False
+element n (Noeud v _ g d) | v == n = True
+                          | otherwise = element n g || element n d
+--Q14
+noeud :: (c -> String) -> (a -> String) -> (c,a) -> String
+noeud fc fv (c, v) = let sc = fc c in fv v ++ " [color=" ++ sc ++ ", fontcolor=" ++ sc ++ "]"
+
+--Q15
+arcs :: Arbre c a -> [(a,a)]
+arcs Feuille                                           = []
+arcs (Noeud _ _ Feuille Feuille)                       = []
+arcs (Noeud v _ Feuille d@(Noeud vd _ _ _))            = (v,vd):arcs d
+arcs (Noeud v _ g@(Noeud vg _ _ _) Feuille)            = (v,vg):arcs g
+arcs (Noeud v _ g@(Noeud vg _ _ _) d@(Noeud vd _ _ _)) = (v,vg):(v,vd):arcs g ++ arcs d
+
+--Q16
+arc :: (a -> String) -> (a,a) -> String
+arc fs (v1, v2) = fs v1 ++ " -> " ++ fs v2
+
+--Q17
+dotise :: String -> (c -> String) -> (a -> String) -> Arbre c a -> String
+dotise s pcs pvs abr = unlines (header ++ map (noeud pcs pvs) (aplatit abr) ++ lArcs ++ footer)
+  where header = ["digraph \"" ++ s ++ "\" {", "node [fontname=\"DejaVu-Sans\", shape=circle]"]
+        lArcs  = map (arc pvs) (arcs abr)
+        footer = ["}"]
+
+
 -- tests
 prop_hauteurPeigne_with_rec xs = length xs == hauteurRec (peigneGauche xs)
 prop_hauteurPeigne_with_fold xs = length xs == hauteurF (peigneGauche xs)
