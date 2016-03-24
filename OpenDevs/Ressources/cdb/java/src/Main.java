b7222c37105bfbddac3f211f1f116976157ccbc7
Kaisow <kaisow@live.fr>
2016-03-24 14:17:42 +0100

    Ajout des tests

--- OpenDevs/Ressources/cdb/java/src/Main.java
+++ OpenDevs/Ressources/cdb/java/src/Main.java
@@ -29,12 +29,19 @@ public class Main {
 	}
 	*/
 		Cdb cdb;
-		try {
+		/*try {
 			cdb = new Cdb("ressources/basefile.cdb");
 			System.out.println(new String( cdb.find("one".getBytes()) ));
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		
+		}*/
+		byte[] b1 = {(byte)0,(byte)128,(byte)255};
+		byte[] b2 = {(byte)0,(byte)0,(byte)0};
+		byte[] b3 = {(byte)15,(byte)33,(byte)57};
+		byte[] b4 = {(byte)255,(byte)255,(byte)255, (byte)255, (byte)255, (byte)255};
+		System.out.println(Cdb.hash(b1));
+		System.out.println(Cdb.hash(b2));
+		System.out.println(Cdb.hash(b3));
+		System.out.println(Cdb.hash(b4));
 	}
 }
